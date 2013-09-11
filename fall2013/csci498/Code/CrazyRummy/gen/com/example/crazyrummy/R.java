c76574175688a759e562a10fe9efeecc7507e7fc
Maria Deslis <mdeslis@gmail.com>
2013-09-10 20:34:28 -0600

    current work 2

--- fall2013/csci498/Code/CrazyRummy/gen/com/example/crazyrummy/R.java
+++ fall2013/csci498/Code/CrazyRummy/gen/com/example/crazyrummy/R.java
@@ -24,36 +24,42 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080015;
-        public static final int add_10_p1=0x7f080007;
-        public static final int add_10_p2=0x7f08000c;
-        public static final int add_10_p3=0x7f080011;
-        public static final int add_5_p1=0x7f080006;
-        public static final int add_5_p2=0x7f08000b;
-        public static final int add_5_p3=0x7f080010;
-        public static final int add_ace_p1=0x7f080008;
-        public static final int add_ace_p2=0x7f08000d;
-        public static final int add_ace_p3=0x7f080012;
-        public static final int finish_game=0x7f080014;
-        public static final int player_1=0x7f080004;
+        public static final int action_settings=0x7f080018;
+        public static final int add_10_p1=0x7f08000a;
+        public static final int add_10_p2=0x7f08000f;
+        public static final int add_10_p3=0x7f080014;
+        public static final int add_5_p1=0x7f080009;
+        public static final int add_5_p2=0x7f08000e;
+        public static final int add_5_p3=0x7f080013;
+        public static final int add_ace_p1=0x7f08000b;
+        public static final int add_ace_p2=0x7f080010;
+        public static final int add_ace_p3=0x7f080015;
+        public static final int finish_game=0x7f080017;
+        public static final int player_1=0x7f080007;
+        public static final int player_1_final_layout=0x7f080004;
         public static final int player_1_layout=0x7f080000;
-        public static final int player_1_score=0x7f080005;
-        public static final int player_2=0x7f080009;
+        public static final int player_1_score=0x7f080008;
+        public static final int player_2=0x7f08000c;
+        public static final int player_2_final_layout=0x7f080005;
         public static final int player_2_layout=0x7f080001;
-        public static final int player_2_score=0x7f08000a;
-        public static final int player_3=0x7f08000e;
+        public static final int player_2_score=0x7f08000d;
+        public static final int player_3=0x7f080011;
+        public static final int player_3__final_layout=0x7f080006;
         public static final int player_3_layout=0x7f080002;
-        public static final int player_3_score=0x7f08000f;
+        public static final int player_3_score=0x7f080012;
         public static final int reset_finish_layout=0x7f080003;
-        public static final int reset_game=0x7f080013;
+        public static final int reset_game=0x7f080016;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_summary=0x7f030001;
         public static final int player_1=0x7f030002;
-        public static final int player_2=0x7f030003;
-        public static final int player_3=0x7f030004;
-        public static final int reset_finish=0x7f030005;
+        public static final int player_1_final=0x7f030003;
+        public static final int player_2=0x7f030004;
+        public static final int player_2_final=0x7f030005;
+        public static final int player_3=0x7f030006;
+        public static final int player_3_final=0x7f030007;
+        public static final int reset_finish=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -70,6 +76,9 @@ public final class R {
         public static final int add_ace_p2=0x7f05000b;
         public static final int add_ace_p3=0x7f050010;
         public static final int app_name=0x7f050000;
+        /**  Summary 
+         */
+        public static final int endgame_message=0x7f050013;
         public static final int finish_game=0x7f050012;
         /**  Player 1 
          */
